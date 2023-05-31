5ca4a3a18dbcd04375cb242986fc5649cc14284d
Juan Keymer <juan@Juan-Huawei>
2023-05-31 10:53:52 +0800

    testing branch testing

--- CPIM.c
+++ CPIM.c
@@ -591,12 +591,13 @@ static void activate (GtkApplication *app, gpointer user_data)
   }
 
 
+
 /* main function */
 int main (int argc, char **argv)
   {
   GtkApplication *app;
   int status;
-  app = gtk_application_new ("keymer.lab.contact_process", G_APPLICATION_FLAGS_NONE);
+  app = gtk_application_new ("keymer.lab.contact_process_ising_model", G_APPLICATION_FLAGS_NONE);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
